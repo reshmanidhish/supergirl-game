571247ba4e84c0401df03532cdc9f8332893f789
Reshma Nidhish <reshmanidhish0@gmail.com>
2023-05-04 00:43:55 +0200

    Adding movements for super girl

--- js/index.js
+++ js/index.js
@@ -1,59 +1,150 @@
-const canvas = document.getElementById("canvas");
-// canvas.style.display = "none";
-const ctx    = canvas.getContext("2d");
-ctx.fillStyle = 'white';
-ctx.font = '18px serif';
+const myObstacles = [];
 
-class Girl {
-  constructor() {
-    this.x = 25;
-    this.y = 25;
-    
-    // Load the image
-    const img = new Image();
-    
-    img.addEventListener('load', () => {
-      // Once image loaded => draw
-      this.img = img;
-      this.draw();
-    });
-    img.src = "/images/supergirl.jpg"
+const myGameArea = {
+  canvas: document.createElement("canvas"),
+  frames: 0,
+  start: function() {
+    this.canvas.width = 480;
+    this.canvas.height = 270;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    // call updateGameArea() every 20 milliseconds
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  clear: function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  stop: function() {
+    clearInterval(this.interval);
+  },
+  score: function() {
+    const points = Math.floor(this.frames / 5);
+    this.context.font = "18px serif";
+    this.context.fillStyle = "black";
+    this.context.fillText(`Score: ${points}`, 350, 50);
   }
-  moveUp() {
-    this.y -= 25;
+};
+
+class Component {
+  constructor(width, height, imageUrl, x, y) {
+    this.width = width;
+    this.height = height;
+    this.imageUrl = imageUrl;
+    this.x = x;
+    this.y = y;
+    // new speed properties
+    this.speedX = 0;
+    this.speedY = 0;
+    this.img = new Image();
   }
-  moveDown() {
-    this.y += 25;
+
+  update() {
+    // let ctx = myGameArea.context;
+    // ctx.fillStyle = this.color;
+    // ctx.fillRect(this.x, this.y, this.width, this.height);
+
+    let ctx = myGameArea.context;
+    this.img.src = this.imageUrl;
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
-  moveLeft() {
-    this.x -= 25;
+
+  newPos() {
+    this.x += this.speedX;
+    this.y += this.speedY;
   }
-  moveRight() {
-    this.x += 25;
+
+  left() {
+    return this.x;
   }
-  draw() {
-    ctx.drawImage(this.img, this.x, this.y, 50, 50);
+  right() {
+    return this.x + this.width;
+  }
+  top() {
+    return this.y;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
+
+  crashWith(obstacle) {
+    return !(
+      this.bottom() < obstacle.top() ||
+      this.top() > obstacle.bottom() ||
+      this.right() < obstacle.left() ||
+      this.left() > obstacle.right()
+    );
   }
 }
 
-const girl = new Girl();
+const player = new Component(50, 50, "/images/supergirl.png", 0, 110);
 
-document.addEventListener('keydown', e => {
+function updateGameArea() {
+  myGameArea.clear();
+  // update the player's position before drawing
+  player.newPos();
+  player.update();
+  // update the obstacles array
+  updateObstacles();
+  // check if the game should stop
+  checkGameOver();
+  // update and draw the score
+  myGameArea.score();
+}
+
+myGameArea.start(); // Starting of the game
+
+document.onkeydown = function(e) {
   switch (e.keyCode) {
-    case 38: girl.moveUp();    console.log('up',    girl); break;
-    case 40: girl.moveDown();  console.log('down',  girl); break;
-    case 37: girl.moveLeft();  console.log('left',  girl); break;
-    case 39: girl.moveRight(); console.log('right', girl); break;
+    case 38: // up arrow
+      player.speedY -= 1;
+      break;
+    case 40: // down arrow
+      player.speedY += 1;
+      break;
+    case 37: // left arrow
+      player.speedX -= 1;
+      break;
+    case 39: // right arrow
+      player.speedX += 1;
+      break;
   }
-  updateCanvas();
-})
+};
 
-function updateCanvas() {
-  ctx.clearRect(0,0,1500,1700);
-  ctx.fillText("girl_x: " + girl.x, 580,40);
-  ctx.fillText("girl_y: " + girl.y, 580,60);
-  
-  girl.draw()
+document.onkeyup = function(e) {
+  player.speedX = 0;
+  player.speedY = 0;
+};
+
+function updateObstacles() {
+  for (i = 0; i < myObstacles.length; i++) {
+    myObstacles[i].x += -1;
+    myObstacles[i].update();
+  }
+
+  myGameArea.frames += 1;
+  if (myGameArea.frames % 120 === 0) {
+    let x = myGameArea.canvas.width;
+    let minHeight = 20;
+    let maxHeight = 200;
+    let height = Math.floor(
+      Math.random() * (maxHeight - minHeight + 1) + minHeight
+    );
+    let minGap = 50;
+    let maxGap = 200;
+    let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+    // myObstacles.push(new Component(10, height, "green", x, 0));
+    myObstacles.push(
+      new Component(30, x - height - gap, "/images/building.webp", x, height + gap)
+    );
+  }
 }
 
-updateCanvas()
+function checkGameOver() {
+  const crashed = myObstacles.some(function(obstacle) {
+    return player.crashWith(obstacle);
+  });
+
+  if (crashed) {
+    myGameArea.stop();
+  }
+}
