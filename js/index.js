84c5e0bbed8e294529acc8eb46f37de601162a74
Reshma Nidhish <reshmanidhish0@gmail.com>
2023-05-06 15:57:30 +0200

    meteors aadded

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ const myGameArea = {
     this.startCanvas.style.display = "block";
     document.body.insertBefore(this.startCanvas, document.body.childNodes[0]);
 
-    // landing page game description along with start button
+    // landing page background image setting 
     const backgroundImage = new Image();
     backgroundImage.src = "/images/background city landing page.png";
     backgroundImage.onload = () => {
@@ -24,11 +24,22 @@ const myGameArea = {
         this.startCanvas.width,
         this.startCanvas.height
       );
+      // to draw the supergirl image
+      const supergirlImage = new Image();
+  supergirlImage.src = "/images/supergirl.png";
+  supergirlImage.onload = () => {
+    // Draw Supergirl's image on the canvas
+    const supergirlWidth = 150;
+    const supergirlHeight = 150;
+    const supergirlX = 0;
+    const supergirlY = (this.startCanvas.height - supergirlHeight) / 2;
+    this.startContext.drawImage(supergirlImage, supergirlX, supergirlY, supergirlWidth, supergirlHeight);
+  };
 
       this.startContext.fillStyle = "black";
       this.startContext.font = "30px Arial";
       this.startContext.fillText(
-        "Click to Start",
+        "Click to start",
         this.startCanvas.width / 2 - 90,
         this.startCanvas.height / 2 - 30
       );
@@ -59,6 +70,9 @@ const myGameArea = {
         this.startCanvas.style.display = "none";
         this.canvas.style.display = "block";
 
+       
+       
+       
         //gameCanvas
         this.canvas.width = 800;
         this.canvas.height = 600;
@@ -157,6 +171,40 @@ class Component {
 }
 
 const player = new Component(50, 50, "/images/supergirl.png", 0, 110);
+class Meteor extends Component {
+    constructor(width, height, imageUrl, x, y) {
+      super(width, height, imageUrl, x, y);
+      this.y = Math.floor(Math.random() * (myGameArea.canvas.height - this.height));
+      this.speedX = 2;
+      this.img.src = "/images/main meteor.png";
+    }
+  }
+  
+  const meteors = [];
+  
+  function updateMeteors() {
+    for (let i = 0; i < meteors.length; i++) {
+      meteors[i].x -= meteors[i].speedX;
+      meteors[i].update();
+      if (player.crashWith(meteors[i])) {
+        player.damage += 1;
+        if (player.damage >= 3) {
+          myGameArea.stop();
+        } else {
+          player.restartPos();
+        }
+        meteors.splice(i, 1);
+        i--;
+      } else if (meteors[i].x + meteors[i].width < 0) {
+        meteors.splice(i, 1);
+        i--;
+      }
+    }
+  
+    if (myGameArea.frames % 120 === 0) {
+      meteors.push(new Meteor(50, 50, "/images/main meteor.png", myGameArea.canvas.width, 0));
+    }
+  }
 
 function updateGameArea() {
   myGameArea.clear();
@@ -172,6 +220,7 @@ function updateGameArea() {
   myGameArea.context.font = "18px serif";
   myGameArea.context.fillStyle = "black";
   myGameArea.context.fillText(`Damage: ${player.damage}`, 20, 50);
+  updateMeteors()
 }
 
 myGameArea.start(); // Starting of the game
