00565acd2b17c6061bbfde8abf440e122bce0d6b
Reshma Nidhish <reshmanidhish0@gmail.com>
2023-05-06 13:38:59 +0200

    damage given

--- js/index.js
+++ js/index.js
@@ -1,60 +1,87 @@
 const myObstacles = [];
 
 const myGameArea = {
-    startCanvas: document.createElement("canvas"),
+  startCanvas: document.createElement("canvas"),
   canvas: document.createElement("canvas"),
   frames: 0,
-  start: function() {
-//landing page canvas
-this.startCanvas.width = 1000;
-this.startCanvas.height = 600;
-this.startContext = this.startCanvas.getContext("2d");
-this.startCanvas.style.display = "block";
-document.body.insertBefore(this.startCanvas, document.body.childNodes[0]);
+  start: function () {
+    //landing page canvas
+    this.startCanvas.width = 1000;
+    this.startCanvas.height = 600;
+    this.startContext = this.startCanvas.getContext("2d");
+    this.startCanvas.style.display = "block";
+    document.body.insertBefore(this.startCanvas, document.body.childNodes[0]);
 
-// landing page game description along with start button
-const backgroundImage = new Image();
+    // landing page game description along with start button
+    const backgroundImage = new Image();
     backgroundImage.src = "/images/background city landing page.png";
     backgroundImage.onload = () => {
       // Draw the image on the starting canvas
-      this.startContext.drawImage(backgroundImage, 0, 0, this.startCanvas.width, this.startCanvas.height);
+      this.startContext.drawImage(
+        backgroundImage,
+        0,
+        0,
+        this.startCanvas.width,
+        this.startCanvas.height
+      );
 
-    this.startContext.fillStyle = "black";
-    this.startContext.font = "30px Arial";
-    this.startContext.fillText("Click to Start", this.startCanvas.width / 2 - 90, this.startCanvas.height / 2 - 30);
-    this.startContext.font = "20px Arial";
-    this.startContext.fillText("Game Description", this.startCanvas.width / 2 - 90, this.startCanvas.height / 2 + 20);
-
-
-    this.startCanvas.addEventListener("click", () => {
-
+      this.startContext.fillStyle = "black";
+      this.startContext.font = "30px Arial";
+      this.startContext.fillText(
+        "Click to Start",
+        this.startCanvas.width / 2 - 90,
+        this.startCanvas.height / 2 - 30
+      );
+      this.startContext.font = "30px Arial";
+      this.startContext.fillText(
+        "Supergirl's Heroic Quest",
+        this.startCanvas.width / 2 - 90,
+        50
+      );
+      this.startContext.font = "16px Arial";
+      this.startContext.fillText(
+        "The game is about a Supergirl which fly through the city using arrow keys,but be careful not to hit the buildings or else ",
+        this.startCanvas.width / 20,
+        80
+      );
+      this.startContext.fillText(
+        "the city will take damage.Also watch out for kryptonite meteors ,which will decrease your health by 50% if you touch them.",
+        this.startCanvas.width / 20,
+        120
+      );
+      this.startContext.fillText(
+        "If you hit three buidings or your health goes to 0%,it's game over.Help Supergirl protect the city and emerge as a true hero.",
+        this.startCanvas.width / 20,
+        160
+      );
+      this.startCanvas.addEventListener("click", () => {
         // to hide canvas
         this.startCanvas.style.display = "none";
-        this.canvas.style.display = "block";   
+        this.canvas.style.display = "block";
 
-    //gameCanvas
-    this.canvas.width = 1000;
-    this.canvas.height = 600;
-    
-    this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    // call updateGameArea() every 20 milliseconds
-    this.interval = setInterval(updateGameArea, 1);
-})
-    }
+        //gameCanvas
+        this.canvas.width = 800;
+        this.canvas.height = 600;
+
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+
+        this.interval = setInterval(updateGameArea, 1);
+      });
+    };
   },
-  clear: function() {
+  clear: function () {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   },
-  stop: function() {
+  stop: function () {
     clearInterval(this.interval);
   },
-  score: function() {
+  score: function () {
     const points = Math.floor(this.frames / 5);
     this.context.font = "18px serif";
     this.context.fillStyle = "black";
     this.context.fillText(`Score: ${points}`, 350, 50);
-  }
+  },
 };
 
 class Component {
@@ -68,19 +95,31 @@ class Component {
     this.speedX = 0;
     this.speedY = 0;
     this.img = new Image();
+    this.damage = 0;
   }
 
   update() {
-    // let ctx = myGameArea.context;
-    // ctx.fillStyle = this.color;
-    // ctx.fillRect(this.x, this.y, this.width, this.height);
-
     let ctx = myGameArea.context;
     this.img.src = this.imageUrl;
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+
+    // damage icon to draw
+    for (let i = 0; i < this.damage; i++) {
+      ctx.fillStyle = "red";
+      ctx.fillRect(this.x + 10 + i * 20, this.y - 20, 10, 10);
+    }
   }
 
   newPos() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  restartPos() {
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = 0;
+    this.y = 110;
     this.x += this.speedX;
     this.y += this.speedY;
   }
@@ -99,12 +138,21 @@ class Component {
   }
 
   crashWith(obstacle) {
-    return !(
+    const crashed = !(
       this.bottom() < obstacle.top() ||
       this.top() > obstacle.bottom() ||
       this.right() < obstacle.left() ||
       this.left() > obstacle.right()
     );
+    if (crashed) {
+      this.damage++;
+      if (this.damage >= 3) {
+        return true;
+      } else {
+        this.restartPos()
+      }
+    }
+    return false;
   }
 }
 
@@ -121,11 +169,14 @@ function updateGameArea() {
   checkGameOver();
   // update and draw the score
   myGameArea.score();
+  myGameArea.context.font = "18px serif";
+  myGameArea.context.fillStyle = "black";
+  myGameArea.context.fillText(`Damage: ${player.damage}`, 20, 50);
 }
 
 myGameArea.start(); // Starting of the game
 
-document.onkeydown = function(e) {
+document.onkeydown = function (e) {
   switch (e.keyCode) {
     case 38: // up arrow
       player.speedY -= 1;
@@ -142,7 +193,7 @@ document.onkeydown = function(e) {
   }
 };
 
-document.onkeyup = function(e) {
+document.onkeyup = function (e) {
   player.speedX = 0;
   player.speedY = 0;
 };
@@ -157,22 +208,28 @@ function updateObstacles() {
   if (myGameArea.frames % 120 === 0) {
     let x = myGameArea.canvas.width;
     let minHeight = 50;
-    let maxHeight = 200;
+    let maxHeight = 100;
     let height = Math.floor(
       Math.random() * (maxHeight - minHeight + 1) + minHeight
     );
-    let minGap = 50;
-    let maxGap = 70;
+    let minGap = 500;
+    let maxGap = 100;
     let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
-    // myObstacles.push(new Component(10, height, "green", x, 0));
+
     myObstacles.push(
-      new Component(200, x - height - gap, "/images/Apartment.png", x, height + gap)
+      new Component(
+        200,
+        x - height - gap,
+        "/images/Apartment.png",
+        x,
+        height + gap
+      )
     );
   }
 }
 
 function checkGameOver() {
-  const crashed = myObstacles.some(function(obstacle) {
+  const crashed = myObstacles.some(function (obstacle) {
     return player.crashWith(obstacle);
   });
 
